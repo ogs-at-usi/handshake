d0787c779a6501b36fde6e579987c0461a0fe8a8
Michele Dalle Rive <dallem@usi.ch>
2022-11-25 23:27:29 +0100

    Updated jwt cookies names.

--- constants/auth.constants.js
+++ constants/auth.constants.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-  JWT_COOKIE_NAME: 'token',
-  REFRESH_COOKIE_NAME: 'refresh',
+  JWT_COOKIE_NAME: 'JWT_TOKEN',
+  REFRESH_COOKIE_NAME: 'REFRESH_TOKEN',
   JWT_MAX_AGE: 5 * 60 * 1000,
   REFRESH_MAX_AGE: 14 * 24 * 60 * 60 * 1000,
 });
