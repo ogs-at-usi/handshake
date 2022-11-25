2fd8b9c2cb7697f32a74947ea3d5580b64d370a5
Michele Dalle Rive <dallem@usi.ch>
2022-11-25 17:28:38 +0100

    Defined jwt token and refresh token cookie options.

--- utils/jwt.utils.js
+++ utils/jwt.utils.js
@@ -1,7 +1,23 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+
+const JWT_TOKEN_COOKIE = {
+  httpOnly: true,
+  sameSite: 'strict',
+  secure: process.env.NODE_ENV === 'production',
+  // expires in 5 minutes
+  maxAge: 5 * 60 * 1000,
+};
+
+const REFRESH_TOKEN_COOKIE = {
+  httpOnly: true,
+  sameSite: 'strict',
+  secure: process.env.NODE_ENV === 'production',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+
 
 function generateJWT(userID) {
-  return jwt.sign({userID}, process.env.JWT_SECRET, { expiresIn: "5m" });
+  return jwt.sign({ userID }, process.env.JWT_SECRET, { expiresIn: '5m' });
 }
 
 async function verifyJWT(token) {
@@ -15,7 +31,12 @@ async function verifyJWT(token) {
   });
 }
 
+function generateRefreshJWT(userID) {}
+
 module.exports = {
+  JWT_TOKEN_COOKIE,
+  REFRESH_TOKEN_COOKIE,
   generateJWT,
-  verifyJWT
-}
+  verifyJWT,
+  generateRefreshJWT,
+};
