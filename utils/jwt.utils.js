c724c4694585dcb830d31ca4345b36ae240aebdf
Michele Dalle Rive <dallem@usi.ch>
2022-11-25 19:47:34 +0100

    Created refresh JWT token functions.

--- utils/jwt.utils.js
+++ utils/jwt.utils.js
@@ -15,7 +15,6 @@ const REFRESH_TOKEN_COOKIE = {
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
-
 function generateJWT(userID) {
   return jwt.sign({ userID }, process.env.JWT_SECRET, { expiresIn: '5m' });
 }
@@ -24,7 +23,40 @@ async function verifyJWT(token) {
   return jwt.verify(token, process.env.JWT_SECRET);
 }
 
-function generateRefreshJWT(userID) {}
+function generateRefreshJWT(userID) {
+  const refreshToken = jwt.sign({ userID }, process.env.REFRESH_SECRET, {
+    expiresIn: '7d',
+  });
+
+  const refresh = {
+    userID,
+    refreshToken,
+  };
+
+  // TODO - update the database with the refresh token
+  // await saveRefreshToken(refresh);
+  console.log(refresh);
+
+  return refreshToken;
+}
+
+function verifyRefreshJWT(refreshToken) {
+  return jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+}
+
+function refreshJWT(refreshToken) {
+  const userID = verifyRefreshJWT(refreshToken);
+
+  // TODO check in the database if the user's refresh token is the one we have
+  const dbRefreshToken = refreshToken; // await getRefreshToken(userID);
+  if (dbRefreshToken !== refreshToken) {
+    throw new Error('Invalid refresh token');
+  }
+
+  const token = generateJWT(userID);
+  const refresh = generateRefreshJWT(userID);
+  return { token, refresh };
+}
 
 module.exports = {
   JWT_TOKEN_COOKIE,
@@ -32,4 +64,6 @@ module.exports = {
   generateJWT,
   verifyJWT,
   generateRefreshJWT,
+  verifyRefreshJWT,
+  refreshJWT,
 };
