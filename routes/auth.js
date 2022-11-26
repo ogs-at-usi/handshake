2b146a3f7bfad653fa83f8d8dc690e9e05517002
Michele Dalle Rive <dallem@usi.ch>
2022-11-26 23:46:43 +0100

    Added database interaction in login and token refresh.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { comparePassword } = require('../utils/auth.utils');
+const { comparePassword, hashPassword } = require('../utils/auth.utils');
 const {
   generateJWT,
   generateRefreshJWT,
@@ -8,9 +8,11 @@ const {
   refreshJWT,
 } = require('../utils/jwt.utils');
 const constants = require('../constants/auth.constants');
+const { User } = require('../models/user');
+const { RefreshToken } = require('../models/refreshToken');
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // retrieve username and password from request
   // username could be either the username or email
   const { username, password } = req.body;
@@ -20,14 +22,12 @@ router.post('/login', (req, res) => {
     });
   }
 
-  // const user = await getUser(username);
-  // original password: "password"
-  const user = {
-    _id: 'eskere',
-    email: 'test@gmail.com',
-    username: 'test',
-    password: '$2b$08$x3b7JL1o1c1QEsl5B5La6.aDDUGPBK9LKpLhkxPZkvNFoZ.RpYKU6',
-  };
+  // get the user that has username = username or email = username
+  console.log(username);
+  const user = await User.findOne({
+    $or: [{ username }, { email: username }],
+  }).exec();
+
   if (!user) {
     return res.status(401).json({
       message: 'Wrong username or password',
@@ -45,7 +45,7 @@ router.post('/login', (req, res) => {
   // generate a JWT token and send it back to the user
 
   const token = generateJWT(user._id);
-  const refresh = generateRefreshJWT(user._id);
+  const refresh = await generateRefreshJWT(user._id);
 
   res
     .cookie(constants.JWT_COOKIE_NAME, token, JWT_TOKEN_COOKIE)
@@ -56,7 +56,7 @@ router.post('/login', (req, res) => {
     });
 });
 
-router.post('/refresh', (req, res) => {
+router.post('/refresh', async (req, res) => {
   const refreshToken = req.cookies[constants.REFRESH_COOKIE_NAME];
   if (!refreshToken) {
     return res.status(401).json({
@@ -65,7 +65,7 @@ router.post('/refresh', (req, res) => {
   }
 
   try {
-    const { token, refresh } = refreshJWT(refreshToken);
+    const { token, refresh } = await refreshJWT(refreshToken);
     res
       .cookie(constants.JWT_COOKIE_NAME, token, JWT_TOKEN_COOKIE)
       .cookie(constants.REFRESH_COOKIE_NAME, refresh, REFRESH_TOKEN_COOKIE)
@@ -80,11 +80,13 @@ router.post('/refresh', (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', async (req, res) => {
   const refreshToken = req.cookies[constants.REFRESH_COOKIE_NAME];
   if (refreshToken) {
-    // TODO - delete the refresh token from the database
-    // await deleteRefreshToken(refreshToken);
+    // delete refresh token from database
+    try {
+      await RefreshToken.findOneAndDelete({ token: refreshToken }).exec();
+    } catch (e) {}
   }
   res
     .clearCookie('token', JWT_TOKEN_COOKIE)
@@ -103,8 +105,17 @@ router.post('/register', (req, res) => {
     });
   }
 
-  // Create user in the db
-  // const user = await createUser(email, username, password)
+  const user = User.create({
+    email,
+    username,
+    password: hashPassword(password),
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: 'Could not create the user',
+    });
+  }
 
   res.status(201).json({
     message: 'User created successfully',
