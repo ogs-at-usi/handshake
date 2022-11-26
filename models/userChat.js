b144e6c5be0a36335f5746f2a4fb9c5d54694642
Michele Dalle Rive <dallem@usi.ch>
2022-11-26 19:22:00 +0100

    Added requirements in userChat file.

--- models/userChat.js
+++ models/userChat.js
@@ -1,6 +1,9 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
 const userChatSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
 });
 
 module.exports.UserChat = mongoose.model('UserChat', userChatSchema);
