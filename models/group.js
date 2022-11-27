67dfeca7b7b4472de8b9c300cb263d77e37cc720
Michele Dalle Rive <dallem@usi.ch>
2022-11-27 12:09:35 +0100

    Added required fields in group model.

--- models/group.js
+++ models/group.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const groupSchema = new Schema({
-    title: String,
-    description: String,
-    chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
 });
 
 module.exports.Group = mongoose.model('Group', groupSchema);
