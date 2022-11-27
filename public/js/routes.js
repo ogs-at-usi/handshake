5c30c48b588892807abe56e9fa0237d48efedf56
Nico <vednditasetam@gmail.com>
2022-11-27 22:38:19 +0100

    main views composed

--- public/js/routes.js
+++ public/js/routes.js
@@ -4,7 +4,10 @@
 function loadMain(){
     
     const body = document.querySelector('body');
-    body.innerHTML = ejs.views_chatcontainer();
+    // body.innerHTML = ejs.views_contactlist();
+
+    body.innerHTML = ejs.views_chatComposer();
+
     console.log('loadMain()');
 
 }
