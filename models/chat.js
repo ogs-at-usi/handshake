cb2baac46af0db98fab023eccddec550558fdfbf
Michele Dalle Rive <dallem@usi.ch>
2022-11-27 12:09:05 +0100

    Added default is_group=false in chat.

--- models/chat.js
+++ models/chat.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const chatSchema = new Schema({
-    is_group: Boolean,
-    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
+  is_group: {
+    type: Boolean,
+    default: false,
+  },
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
 });
 
 module.exports.Chat = mongoose.model('Chat', chatSchema);
