71ced5cd00ef0066bbe91214133818c8111b975c
Michele Dalle Rive <dallem@usi.ch>
2022-11-27 10:52:10 +0100

    Made the user name field required and unique.

--- models/user.js
+++ models/user.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    password: String,
-    email: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: String,
+  email: String,
 
-    chats: [{ type: Schema.Types.ObjectId, ref: 'Chat' }],
+  chats: [{ type: Schema.Types.ObjectId, ref: 'Chat' }],
 });
 
 module.exports.User = mongoose.model('User', userSchema);
