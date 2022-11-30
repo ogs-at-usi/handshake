bbed207e4d79d7f9f747be7aeadeb81a4ffc3d72
Jeferson Morales Mariciano <moralj@usi.ch>
2022-11-30 16:31:03 +0100

    Temporary ignoring linting `unused`

--- public/js/classes/group.js
+++ public/js/classes/group.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 class Group extends Chat {
     _id = null;
     title = null;
     description = null;
 
     constructor({ _id = null, members = null, messages = null, title = null, description = null }) {
-        super({ _id, members, messages });
+        super({ _id, members, messages, isGroup: true });
         this.title = title;
         this.description = description;
     };
