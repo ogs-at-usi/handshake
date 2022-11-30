7fb4ac8e9611110c05b43466bcdc69d8a437d534
Jeferson Morales Mariciano <moralj@usi.ch>
2022-11-30 16:07:14 +0100

    Linting resolves
    besides `never-used` linting problem

--- public/js/classes/message.js
+++ public/js/classes/message.js
@@ -4,18 +4,18 @@ class Message {
     chat = null;
     type = null;
     content = null;
-    sent_at = null;
-    delivered_at = null;
+    sentAt = null;
+    deliveredAt = null;
     seen = null;
 
-    constructor({ _id = null, sender = null, chat = null, type = null, content = null, sent_at = null, delivered_at = null, seen = null }) {
+    constructor({ _id = null, sender = null, chat = null, type = null, content = null, sentAt = null, deliveredAt = null, seen = null }) {
         this._id = _id;
         this.sender = sender;
         this.chat = chat;
         this.type = type;
         this.content = content;
-        this.sent_at = sent_at;
-        this.delivered_at = delivered_at;
+        this.sent_at = sentAt;
+        this.delivered_at = deliveredAt;
         this.seen = seen;
     };
 };
