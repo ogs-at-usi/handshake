f1c86f17f7f9e77fd46f0075fca7c021604dea6d
Michele Dalle Rive <dallem@usi.ch>
2022-12-01 02:24:58 +0100

    Made express serve Vue also on development.

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  outputDir: '../server/public',
+  indexPath: './index.html',
+  devServer: {
+    host: 'localhost',
+    hot: 'only',
+    devMiddleware: {
+      publicPath: 'http://localhost:8888/',
+      writeToDisk: true,
+    },
+  },
 });
