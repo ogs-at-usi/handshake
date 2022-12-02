5fb16dabeab403bd32dc0d414a5eff0c4d4b2f71
Michele Dalle Rive <dallem@usi.ch>
2022-12-02 11:38:08 +0100

    Fixed prettier.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
