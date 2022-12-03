32f9ae404af0469ec0da8295ffa79165c3b9aa9f
Michele Dalle Rive <dallem@usi.ch>
2022-12-03 16:51:18 +0100

    Frontend: Made the api client a plugin.

--- client/src/plugin/api-client.js
+++ client/src/plugin/api-client.js
@@ -3,17 +3,45 @@ class ApiClient {
     this.axiosInstance = axiosInstance;
   }
 
+  /**
+   * Retrieve the list of users that match the filter
+   * @param filter {string} The filter to apply
+   * @returns {Promise<AxiosResponse<any>>} The promise with the response
+   */
   getUsers(filter) {
     return this.axiosInstance.get(`/users?filter=${filter}`);
   }
 
+  /**
+   * Create a chat with the logged user and the user with the given ID
+   * @param otherUserID {string} The ID of the other user
+   * @returns {Promise<AxiosResponse<any>>} The promise with the response
+   */
   createChat(otherUserID) {
     return this.axiosInstance.post('/chats', { otherUserID });
   }
 
+  /**
+   * Create a message in a chat
+   * @param chatID {string} The ID of the chat
+   * @param message {Message} The message to create
+   * @returns {Promise<AxiosResponse<any>>} The promise with the response
+   */
   sendMessage(chatID, message) {
-    return this.axiosInstance.post(`/chats/${chatID}/messages`, { message });
+    return this.axiosInstance.post(`/chats/${chatID}/messages`, {
+      type: message.type,
+      content: message.content,
+    });
   }
 }
 
-export default ApiClient;
+export default {
+  /**
+   * Install the plugin
+   * @param Vue {Vue} The Vue instance
+   * @param axiosInstance {AxiosInstance} The axios instance
+   */
+  install(Vue, axiosInstance) {
+    Vue.prototype.$api = new ApiClient(axiosInstance);
+  },
+};
