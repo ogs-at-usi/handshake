c7a1952f16224ca2ae2904d98e5945b84c77c198
Jeferson Morales Mariciano <moralj@usi.ch>
2022-12-04 20:11:23 +0100

    Frontend: ChatMessage.vue refactor
    added computed methods for retrieve:
    - isSelf: if the user and the message sender are the same
    - isGroup: if the chat the message is on is a group chat or not
    - selfClass: a string containing the `self` css class
    - timestamp: distinguish whether is the message timestamp should be the `sentAt` field for the message sent by the user, or the `deliveredAt` if the message is being sent by another chat member
    - senderName: retrieves the sender name to add to the header message component.

--- client/src/components/ChatMessage.vue
+++ client/src/components/ChatMessage.vue
@@ -1,17 +1,14 @@
 <template>
   <!-- UI: MESSAGE -->
-  <div
-    :id="message._id"
-    :class="`card ${message.sender === $store.getters.user._id ? 'self' : ''}`"
-  >
+  <div :id="message._id" :class="`card ${selfClass}`">
     <!-- message with header of name of sender (none if ours) and message text -->
     <div class="card-body">
-      <!--      <h5 class="card-title">{{ otheruser.name }}</h5>-->
-      <p class="card-text">{{ message.content }}</p>
+      <h5 class="card-title">{{ senderName }}</h5>
+      <p class="card-text">{{ content }}</p>
     </div>
 
     <!-- timestamp of the message: sent_at for our message, delivered_at for others -->
-    <p class="timestamp text-end">{{ message.deliveredAt }}</p>
+    <p class="timestamp text-end">{{ timestamp }}</p>
   </div>
 </template>
 
@@ -24,6 +21,33 @@ export default {
     message: {
       type: Message,
       required: true,
+    },
+  },
+  computed: {
+    isSelf() {
+      return this.$props.message.sender === this.$store.getters.user._id;
+    },
+    isGroup() {
+      // TODO: M3 pass also the chat class
+      // this.$props.chat instanceof Group
+      return null;
+    },
+    selfClass() {
+      return this.isSelf ? 'self' : '';
+    },
+    timestamp() {
+      return this.isSelf ? this.$props.message.sentAt : this.$props.message.deliveredAt;
+    },
+    content() {
+      const retrieveContent = {};
+      // TODO: substitute with enum field
+      retrieveContent.TEXT = this.$props.message.content;
+      return retrieveContent[this.$props.message.type];
+    },
+    senderName() {
+      if (this.isGroup) return null; // this.$props.chat.title;
+      console.error('this function should never be reached', this.$props.message);
+      return '';
     },
   },
 };
