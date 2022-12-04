996cb2c09c4b241b16b7fd3ba7beca89a175208a
Loredana De Filippo <defill@usi.ch>
2022-12-04 13:03:23 +0100

    Frontend: Fixed issues and added props

--- client/src/components/ChatMessage.vue
+++ client/src/components/ChatMessage.vue
@@ -6,22 +6,27 @@
   >
     <!-- message with header of name of sender (none if ours) and message text -->
     <div class="card-body">
-      <h5 class="card-title">{{ otheruser.name }}</h5>
+      <!--      <h5 class="card-title">{{ otheruser.name }}</h5>-->
       <p class="card-text">{{ message.content }}</p>
     </div>
 
     <!-- timestamp of the message: sent_at for our message, delivered_at for others -->
-    <p class="timestamp text-end">{{ message.delivered_at }}</p>
-
+    <p class="timestamp text-end">{{ message.deliveredAt }}</p>
   </div>
 </template>
 
 <script>
+import Message from '@/classes/message';
+
 export default {
   name: 'ChatMessage',
+  props: {
+    message: {
+      type: Message,
+      required: true,
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
