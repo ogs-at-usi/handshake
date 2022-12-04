a76a5f004ca255995161cd43cf19825e0fa126f8
Jeferson Morales Mariciano <moralj@usi.ch>
2022-12-05 00:07:42 +0100

    Frontend: Added chats create method.
    If the chat created is the chat the user just created, display it.

--- client/src/views/AppContainer.vue
+++ client/src/views/AppContainer.vue
@@ -32,16 +32,33 @@ export default {
     const socket = io(':8888');
     console.log('Trying to connect');
     this.$store.commit('setSocket', socket);
+
     socket.on('chats:read', (chats) => {
-      console.log('Chats received', chats);
+      console.log('EVENT chats:read -', chats);
       this.chats = chats.map((chat) => new Chat(chat));
       // this.setActiveChat(this.chats[0]._id);
     });
+
     socket.on('messages:create', (message) => {
-      console.log('Message received', message);
+      console.log('EVENT messages:create -', message);
       const chatId = message.chat;
       const chat = this.chats.find((chat) => chat._id === chatId);
       chat.messages.push(new Message(message));
+    });
+
+    /**
+     * Get newly created chat.
+     */
+    socket.on('chats:create', chatJson => {
+      console.log('EVENT chats:create -', chatJson);
+      const chat = new Chat(chatJson);
+      this.chats.unshift(chat);
+
+      console.log(chat._id, this.activeChat._id);
+      if (chat.members[0]._id === this.$store.getters.user._id) {
+        console.log(chat);
+        this.activeChat = chat;
+      }
     });
   },
   methods: {
@@ -51,10 +68,14 @@ export default {
     },
     userSelected(otherUser) {
       this.activeChat = new Chat({
-        members: [otherUser],
+        members: [otherUser, this.$store.getters.user],
         messages: [],
       });
     },
+    overrideChat(id) {
+      console.log(id);
+      this.activeChat._id = id;
+    }
   },
   components: { AppMenu, ChatBoard },
 };
