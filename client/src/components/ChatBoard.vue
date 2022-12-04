fdd12e8c872c283b44e46b40520cb837e0e5ef82
Loredana De Filippo <defill@usi.ch>
2022-12-04 12:57:33 +0100

    Frontend: Appended the messages

--- client/src/components/ChatBoard.vue
+++ client/src/components/ChatBoard.vue
@@ -3,16 +3,33 @@
     <header>
       <!-- image & name of the chat: other user image or group image -->
       <img class="pfp" src="icons/default_pfp.png" alt="pfp" />
-      <h2>{{ chat instanceof Group ? chat.title : otheruser.name }}</h2>
+      <!--      <h2>{{ // chat instanceof Group ? chat.title : otheruser.name }}</h2>-->
     </header>
 
     <main class="d-flex flex-column">
-      <!-- append messages inside here -->
+      <!--       append messages inside here-->
+      <ChatMessage
+        v-for="(message, index) in chat.messages"
+        :key="index"
+        :message="message"
+      ></ChatMessage>
+      <!--      <ChatMessage-->
+      <!--        :message="{-->
+      <!--          _id: '1',-->
+      <!--          content: 'Hello',-->
+      <!--          deliveredAt: '2021-05-01T00:00:00.000Z',-->
+      <!--        }"-->
+      <!--      />-->
     </main>
 
     <footer class="d-flex row align-items-center justify-content-between">
       <form id="search-bar" action="">
-        <input type="text" name="message" placeholder="Write something..." class="col-9"/>
+        <input
+          type="text"
+          name="message"
+          placeholder="Type something..."
+          class="col-9"
+        />
         <button>💬</button>
       </form>
     </footer>
@@ -20,21 +37,19 @@
 </template>
 
 <script>
-import Chat from '@/classes/chat'
-// eslint-disable-next-line no-unused-vars
-import Group from '@/classes/group'
+// import Chat from '@/classes/chat';
+import ChatMessage from '@/components/ChatMessage';
 
 export default {
   name: 'ChatBoard',
+  components: { ChatMessage },
   props: {
     chat: {
-      type: Chat,
+      type: Object,
       required: true,
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
