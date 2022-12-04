3b315a0d85574dd2ecea645b801bfd983b45d9bf
Loredana De Filippo <defill@usi.ch>
2022-12-04 13:01:51 +0100

    Frontend: Added the active chat

--- client/src/views/AppContainer.vue
+++ client/src/views/AppContainer.vue
@@ -1,20 +1,26 @@
 <template>
-  <div id="app_container">
+  <div id="app_container" class="justify-content-between d-flex flex-row h-100">
     <!-- content for the left hand side of the app main page -->
     <!-- about profile contact and image, search bar and contact chat list -->
     <ContactList></ContactList>
     <!-- content for the right hand side of the app main page -->
     <!-- chat board containing the chat header, messages and input bar -->
-    <ChatBoard></ChatBoard>
+    <ChatBoard :chat="{}"></ChatBoard>
   </div>
 </template>
 
 <script>
 import ContactList from '@/components/ContactList.vue';
 import ChatBoard from '@/components/ChatBoard.vue';
+// import Chat from '@/classes/chat';
 
 export default {
   name: 'AppContainer',
+  data() {
+    return {
+      activeChat: null,
+    };
+  },
   components: { ContactList, ChatBoard },
 };
 </script>
