0aa3d9e431e9c1e47b1150910c6f44129157b627
Loredana De Filippo <defill@usi.ch>
2022-12-04 22:33:26 +0100

    Frontend: Created the socket to append messages

--- client/src/views/AppContainer.vue
+++ client/src/views/AppContainer.vue
@@ -2,7 +2,11 @@
   <div id="app_container" class="justify-content-between d-flex flex-row h-100">
     <!-- content for the left hand side of the app main page -->
     <!-- about profile contact and image, search bar and contact chat list -->
-    <AppMenu :chats="chats" @selectChat='setActiveChat($event)' @userSelected='userSelected($event)'></AppMenu>
+    <AppMenu
+      :chats="chats"
+      @selectChat="setActiveChat($event)"
+      @userSelected="userSelected($event)"
+    ></AppMenu>
     <!-- content for the right hand side of the app main page -->
     <!-- chat board containing the chat header, messages and input bar -->
     <ChatBoard :chat="activeChat"></ChatBoard>
@@ -14,6 +18,7 @@ import AppMenu from '@/components/AppMenu.vue';
 import ChatBoard from '@/components/ChatBoard.vue';
 import Chat from '@/classes/chat';
 import { io } from 'socket.io-client';
+import Message from '@/classes/message';
 
 export default {
   name: 'AppContainer',
@@ -24,18 +29,24 @@ export default {
     };
   },
   mounted() {
-    const socket = io("localhost:8888");
-    console.log("Trying to connect");
-    this.$store.commit("setSocket", socket);
-    socket.on("chats:read", (chats) => {
-      console.log("Chats received", chats);
+    const socket = io('localhost:8888');
+    console.log('Trying to connect');
+    this.$store.commit('setSocket', socket);
+    socket.on('chats:read', (chats) => {
+      console.log('Chats received', chats);
       this.chats = chats.map((chat) => new Chat(chat));
       // this.setActiveChat(this.chats[0]._id);
+    });
+    socket.on('messages:create', (message) => {
+      console.log('Message received', message);
+      const chatId = message.chat;
+      const chat = this.chats.find((chat) => chat._id === chatId);
+      chat.messages.push(new Message(message));
     });
   },
   methods: {
     setActiveChat(chat) {
-      console.log("EVENT Active chat - ", chat);
+      console.log('EVENT Active chat - ', chat);
       this.activeChat = chat;
     },
     userSelected(otherUser) {
