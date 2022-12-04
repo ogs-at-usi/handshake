b49ffc93978af88d456d43904a6559d58a3158d1
Loredana De Filippo <defill@usi.ch>
2022-12-04 16:38:36 +0100

    Frontend: Changed the type to Chat

--- client/src/components/ChatBoard.vue
+++ client/src/components/ChatBoard.vue
@@ -31,8 +31,8 @@
 </template>
 
 <script>
-// import Chat from '@/classes/chat';
 import ChatMessage from '@/components/ChatMessage';
+import Chat from '@/classes/chat';
 
 export default {
   name: 'ChatBoard',
@@ -44,7 +44,7 @@ export default {
   },
   props: {
     chat: {
-      type: Object,
+      type: Chat,
       required: true,
     },
   },
