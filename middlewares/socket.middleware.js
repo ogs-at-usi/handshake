26a7b5c5d1d07f6f2d227ae9e4e33ad1d0e0646d
Michele Dalle Rive <dallem@usi.ch>
2022-12-05 13:11:43 +0100

    Backend: Added check for jwt token when using socket.

--- middlewares/socket.middleware.js
+++ middlewares/socket.middleware.js
@@ -6,9 +6,12 @@ const authConstants = require('../constants/auth.constants');
 // and if it is, it adds the user id to the socket object
 const authMiddleware = async (socket, next) => {
   const jwtCookieName = authConstants.JWT_COOKIE_NAME;
-  const cookies = cookie.parse(socket.request.headers.cookie);
-  const jwtToken = cookies[jwtCookieName];
   try {
+    const cookies = cookie.parse(socket.request.headers.cookie);
+    const jwtToken = cookies[jwtCookieName];
+    if (!jwtToken) {
+      return next(new Error('Authentication error'));
+    }
     const { userId } = await verifyJWT(jwtToken);
     socket.userId = userId;
     next();
