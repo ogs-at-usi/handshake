6f0572863a4656f2ab3ac68bb736fb38a82fba09
Sofia d'Atri <datris@usi.ch>
2022-12-08 20:36:21 +0100

    updated message utils

--- utils/message.utils.js
+++ utils/message.utils.js
@@ -2,18 +2,17 @@ const { UserChat } = require('../models/userChat');
 const { ObjectId } = require('mongodb');
 const { Chat } = require('../models/chat');
 const { Message } = require('../models/message'); // MessageType is used for verification
-const io = require('../serverSocket').io;
 
 
-async function sendMessage(chatId, userId, message, type, params, body) {
+async function saveMessage(chatId, userId, content, type) {
     const result = await UserChat.findOne({
         chat: new ObjectId(chatId),
         user: new ObjectId(userId),
     });
 
-    console.log(
-        `Route: /chat/:chatId/messages, params: ${params}, body: ${body} - Chat of the new message: ${chatId}`
-    );
+    // console.log(
+    //     `Route: /chat/:chatId/messages, params: ${params}, body: ${body} - Chat of the new message: ${chatId}`
+    // );
 
     if (!result) {
         return null;
@@ -23,7 +22,7 @@ async function sendMessage(chatId, userId, message, type, params, body) {
         sender: ObjectId(userId),
         chat: ObjectId(chatId),
         type,
-        content: message.content,
+        content,
         sentAt: new Date(),
         deliveredAt: new Date(),
     });
@@ -35,10 +34,9 @@ async function sendMessage(chatId, userId, message, type, params, body) {
     chat.messages.push(newMessage._id);
     await chat.save();
 
-    // socket io emit to all users in the chat room that a new message has been created and send the new message
-    io.to(chatId).emit('messages:create', newMessage);
+    
 }
 
 module.exports = {
-    sendMessage,
+    saveMessage,
 };
