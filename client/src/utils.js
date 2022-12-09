28afb5db7dbf8573b6136750df7e1245c6c8afce
Michele Dalle Rive <dallem@usi.ch>
2022-12-09 10:25:21 +0100

    Frontend: Added timestamp formatting function in utils file.

---
+++ client/src/utils.js
@@ -0,0 +1,7 @@
+export function formatTime(time) {
+  const datetime = new Date(time);
+  const hours = datetime.getHours().toString().padStart(2, '0');
+  const minutes = datetime.getMinutes().toString().padStart(2, '0');
+  const seconds = datetime.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+}
