7221adf76157de6b1a173ba1fde7070d41053ac2
Michele Dalle Rive <dallem@usi.ch>
2022-12-09 16:07:56 +0100

    Frontend: Created vuetify themes file.

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -8,31 +8,6 @@ export default new Vuetify({
     options: {
       customProperties: true,
     },
-    themes: {
-      light: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-        surface: '#c5c5c5',
-        textOnPrimary: '#ffffff',
-        textOnAccent: '#000000',
-      },
-      dark: {
-        text: '#ffffff',
-        primary: '#00528c',
-        secondary: '#0d3f62',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-        surface: '#162d3d',
-        background: '#0f1d27',
-      },
-    },
+    themes: {},
   },
 });
