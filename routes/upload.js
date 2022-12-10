1e5de0e475fe63de711c1ef4ec6f2035879e6b3f
Sofia d'Atri <datris@usi.ch>
2022-12-10 19:31:49 +0100

    added audio upload

--- routes/upload.js
+++ routes/upload.js
@@ -6,9 +6,10 @@ const sharp = require('sharp');
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage, limits: { fileSize: 50000000 } }); // 50MB
-const uploadVideoDisk = multer({ dest: "./media/temp", limits: { fileSize: 50000000 } }); // 50MB
+const uploadDisk = multer({ dest: "./media/temp", limits: { fileSize: 50000000 } }); // 50MB
 const { ObjectId } = require('mongodb');
 const { saveMessage } = require('../utils/message.utils');
+const FluentFfmpeg = require('fluent-ffmpeg');
 
 router.post('/image', upload.single('image'), async function (req, res, next) {
     // TODO: change req.body.chatId to whatever the chatId is
@@ -43,7 +44,7 @@ router.post('/image', upload.single('image'), async function (req, res, next) {
 
 });
 
-router.post('/video', uploadVideoDisk.single('video'), async function (req, res, next) {
+router.post('/video', uploadDisk.single('video'), async function (req, res, next) {
     // TODO: change req.body.chatId to whatever the chatId is
     const chatId = req.body.chatId;
     const userId = req.userId;
@@ -74,7 +75,7 @@ router.post('/video', uploadVideoDisk.single('video'), async function (req, res,
             video.setVideoFormat('mp4').save(path, function (error, file) {
                 if (!error)
                     console.log('Video file: ' + file);
-                
+
             });
         }, function (err) {
             console.log('Error: ' + err);
@@ -88,30 +89,92 @@ router.post('/video', uploadVideoDisk.single('video'), async function (req, res,
         if (err) {
             throw err
         }
-
-        console.log(`${dir} is deleted!`)
+        console.log(`${dir} is deleted!`);
     });
 
     res.status(201).json(file);
 });
 
-router.post('/audio', upload.single('audio'), async function (req, res) {
-    const title = req.body.title;
+router.post('/audio', uploadDisk.single('audio'), async function (req, res, next) {
+
+    // TODO: change req.body.chatId to whatever the chatId is
+    const chatId = req.body.chatId;
+    const userId = req.userId;
     const file = req.file;
+    if (!file) {
+        const error = new Error("Please upload a file");
+        error.httpStatusCode = 400;
+        return next(error);
+    }
+
+    if (!ObjectId.isValid(chatId)) {
+        return res.status(400).end();
+    }
+
+    const newMessage = await saveMessage(chatId, userId, '', 'VIDEO');
+
+    if (!newMessage) {
+        return res.status(404).end();
+    }
 
 
+    const path = './media/audio/' + newMessage._id + '.mp3';
+    const dir = './media/temp';
 
-    console.log(title);
-    console.log(file);
+
+    FluentFfmpeg(file.destination + '/' + file.filename)
+        .toFormat('mp3')
+        .on('error', (err) => {
+            console.log('An error occurred: ' + err.message);
+        })
+        .on('progress', (progress) => {
+            // console.log(JSON.stringify(progress));
+            console.log('Processing: ' + progress.targetSize + ' KB converted');
+        })
+        .on('end', () => {
+            console.log('Processing finished !');
+            fs.rmdir(dir, { recursive: true }, err => {
+                if (err) {
+                    throw err
+                }
+                console.log(`${dir} is deleted!`);
+            });
+        })
+        .save(path);
+
+
+    res.status(201).json(file);
 });
 
-router.post('/file', upload.single('file'), async function (req, res) {
-    const title = req.body.title;
+router.post('/file', upload.single('file'), async function (req, res, next) {
+
+    // TODO: change req.body.chatId to whatever the chatId is
+    const chatId = req.body.chatId;
+    const userId = req.userId;
     const file = req.file;
+    if (!file) {
+        const error = new Error("Please upload a file");
+        error.httpStatusCode = 400;
+        return next(error);
+    }
 
+    if (!ObjectId.isValid(chatId)) {
+        return res.status(400).end();
+    }
 
-    console.log(title);
+    const newMessage = await saveMessage(chatId, userId, '', 'VIDEO');
+
+    if (!newMessage) {
+        return res.status(404).end();
+    }
+
+    const extension = req.file.originalname.split('.').pop();
+    const path = './media/files/' + newMessage._id + extension;
+
+    console.log(path);
     console.log(file);
+
+    res.status(201).json(file);
 });
 
 
