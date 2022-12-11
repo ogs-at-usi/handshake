51c0c96aa5a33e0e7a50f735f1a532aa744df524
Loredana De Filippo <defill@usi.ch>
2022-12-11 20:18:42 +0100

    Frontend: Updated the container and appsettings

--- client/src/components/AppMenu.vue
+++ client/src/components/AppMenu.vue
@@ -1,7 +1,10 @@
 <template>
   <!-- MENU WITH OWN IMAGE, SEARCH BAR AND CONTACTS LIST -->
-  <v-container class="pa-0 w-100 ma-0 h-100" fluid>
-    <AppSettings v-if="openSettings" />
+  <v-container class="pa-0 w-100 ma-0 h-100" fluid style="position: relative">
+    <AppSettings
+      class="w-100"
+      v-if="openSettings"
+      @setSettings="openSettings = $event" />
     <v-navigation-drawer clipped color="surface" permanent width="100%">
       <v-list class="pt-0" flat>
         <v-list-item class="pt-2 secondary">
