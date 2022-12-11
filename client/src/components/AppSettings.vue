73ebbea4d04fa21c3c6bfa860fa81e55af9bbce4
Loredana De Filippo <defill@usi.ch>
2022-12-11 20:37:17 +0100

    Frontend: fixed layout

--- client/src/components/AppSettings.vue
+++ client/src/components/AppSettings.vue
@@ -15,23 +15,22 @@
             {{ $store.getters.user.name }}
           </v-list-item-title>
         </v-list-item-content>
-        <v-divider class="visible"></v-divider>
-
-        <v-list dense>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-            @click="item.action">
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
       </v-list-item>
+      <v-divider class="visible"></v-divider>
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          @click="item.action">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-list>
   </v-navigation-drawer>
 </template>
