a191c238d5585cdd16c2eee9203bb86d7d99d5d3
Jeferson Morales Mariciano <moralj@usi.ch>
2022-12-12 08:00:57 +0100

    Frontend: socket `chats:create` supports group
    Added check to see whether the newly created chat in `chats:create` is a private `Chat` or a `Group`

--- client/src/views/AppContainer.vue
+++ client/src/views/AppContainer.vue
@@ -17,6 +17,7 @@
 import AppMenu from '@/components/AppMenu';
 import ChatBoard from '@/components/ChatBoard';
 import { Chat } from '@/classes/chat';
+import { Group } from '@/classes/group';
 import { Message } from '@/classes/message';
 import { io } from 'socket.io-client';
 
@@ -59,7 +60,8 @@ export default {
      */
     socket.on('chats:create', chatJSON => {
       console.log('EVENT chats:create -', chatJSON);
-      const chat = new Chat(JSON.parse(chatJSON));
+      const parsed = JSON.parse(chatJSON);
+      const chat = parsed.isGroup ? new Group(parsed) : new Chat(parsed);
       this.chats.unshift(chat);
 
       if (chat.members[0]._id === this.$store.getters.user._id) {
