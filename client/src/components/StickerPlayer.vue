fba09cba72d762b4c9823ff35e46a6457e640f53
Michele Dalle Rive <dallem@usi.ch>
2022-12-16 12:11:16 +0100

    Frontend: Created sticker player component.
    Currently it only displays the sticker without animating it.

---
+++ client/src/components/StickerPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <lottie-player
+    :src="`/stickers/${sticker}.json`"
+    background="transparent"
+    speed="1"
+    style="width: 100px; height: 100px" />
+</template>
+
+<script>
+import '@lottiefiles/lottie-player';
+export default {
+  name: 'StickerPlayer',
+  props: {
+    sticker: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
