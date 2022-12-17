90cc1474a8cd04f38c1798c9043b35653a739f5a
Loredana De Filippo <defill@usi.ch>
2022-12-17 12:40:31 +0100

    Frontend: Prettier

--- serverSocket.js
+++ serverSocket.js
@@ -80,13 +80,11 @@ function init(server, onlineUsers) {
           console.log(success);
         }
       });
-      socket.broadcast
-        .to(chatId)
-        .emit('messages:update:read', {
-          chatId,
-          lastMessageTime,
-          userId: socket.userId,
-        });
+      socket.broadcast.to(chatId).emit('messages:update:read', {
+        chatId,
+        lastMessageTime,
+        userId: socket.userId,
+      });
     });
 
     onlineUsers.add(socket.userId);
