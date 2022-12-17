2be6bc6fdeb5a3f6f560b0cf1abc6250ee999c23
Loredana De Filippo <defill@usi.ch>
2022-12-17 12:44:08 +0100

    Frontend: Took away the simplified function to see if it works (it does)

--- client/src/views/AppContainer.vue
+++ client/src/views/AppContainer.vue
@@ -29,7 +29,7 @@ import {
   askNotificationPermission,
   sendNotification,
 } from '@/utils/notification.utils';
-import { userSeenMessage } from '@/utils/seen.utils';
+// import { userSeenMessage } from '@/utils/seen.utils';
 
 export default {
   name: 'AppContainer',
@@ -81,13 +81,13 @@ export default {
       this.chats = this.chats.filter((chat) => chat._id !== chatId);
       this.chats.unshift(chat);
       if (this.activeChat && this.activeChat._id === chatId) {
-        // socket.emit('messages:update:read', {
-        //   chatId,
-        //   lastMessageTime: newMessage.deliveredAt,
-        // });
-
-        const lastMessageTime = newMessage.deliveredAt;
-        userSeenMessage(socket, chatId, lastMessageTime);
+        socket.emit('messages:update:read', {
+          chatId,
+          lastMessageTime: newMessage.deliveredAt,
+        });
+        //
+        // const lastMessageTime = newMessage.deliveredAt;
+        // userSeenMessage(socket, chatId, lastMessageTime);
 
         this.$nextTick(() => {
           this.$refs.chatBoard.scrollDown();
