6119d403ba4430f2ee6788418d19c87a23a6bd3e
Jeferson Morales Mariciano <moralj@usi.ch>
2022-12-18 19:59:08 +0100

    Refactor: async/await flattening

--- bin/dropdb.js
+++ bin/dropdb.js
@@ -9,40 +9,38 @@ if (!URL) {
 }
 console.log('Dropping DB at URL: ' + URL);
 
-// ask for confirmation
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-readline.question(
+
+// ask for confirmation
+await readline.question(
   'Are you sure you want to drop the database? (y/n) ',
-  (answer) => {
-    if (answer === 'y') {
-      console.log('Dropping database...');
-      mongoose
-        .connect(URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        })
-        .then(() => {
-          mongoose.connection.db
-            .dropDatabase()
-            .then(() => {
-              console.log('✅ Database dropped');
-              process.exit(0);
-            })
-            .catch((err) => {
-              console.error('Error dropping database: ' + err);
-              process.exit(1);
-            });
-        })
-        .catch((err) => {
-          console.error('Error connecting to database: ' + err);
-          process.exit(1);
-        });
-    } else {
+  async answer => {
+    if (answer !== 'y') {
       console.log('Aborting');
       process.exit(0);
+    }
+
+    console.log('Dropping database...');
+    try {
+      await mongoose.connect(URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+    } catch (err) {
+      console.error('Error connecting to database: ' + err);
+      process.exit(1);
+    }
+
+    try {
+      await mongoose.connection.db.dropDatabase();
+      console.log('✅ Database dropped');
+      process.exit(0);
+    } catch (err) {
+      console.error('Error dropping database: ' + err);
+      process.exit(1);
     }
   }
 );
