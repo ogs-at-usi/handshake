2ce8bf7c5a783cd722727b2cb913ec6ecedd0f31
Michele Dalle Rive <dallem@usi.ch>
2022-12-19 09:48:51 +0100

    Frontend: Created Message view for position messages.

---
+++ client/src/components/message/ChatMessagePosition.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ message.content }}</span>
+</template>
+
+<script>
+export default {
+  name: 'ChatMessagePosition',
+  props: {
+    message: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
