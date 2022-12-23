fb12277f49bdcd3ab8b99412598672b2ab31b8fe
Michele Dalle Rive <dallem@usi.ch>
2022-12-23 13:53:05 +0100

    Frontend: Setup vue gtag in main.js file with google tracking code.

--- client/src/main.js
+++ client/src/main.js
@@ -8,10 +8,14 @@ import apiClient from './plugins/api-client';
 import vuetify from './plugins/vuetify';
 import VuePeerJS from 'vue-peerjs';
 import Peer from 'peerjs';
+import VueGtag from 'vue-gtag';
 
 Vue.use(apiClient, axios);
 Vue.use(VueAxios, axios);
 Vue.use(VuePeerJS, new Peer({}));
+Vue.use(VueGtag, {
+  config: { id: 'G-HB1WBXH9CV' },
+})
 
 Vue.config.productionTip = false;
 
